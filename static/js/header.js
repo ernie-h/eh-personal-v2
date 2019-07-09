3476797d72e009db267a89645035ead89f687bf5
ernie-hao <pdphiro@gmail.com>
2019-07-08 23:02:43 -0400

    skills in config

--- static/js/header.js
+++ static/js/header.js
@@ -2,12 +2,12 @@ window.addEventListener('scroll', function (e) {
   if (window.scrollY >= document.querySelector(".Hero").scrollHeight) {
     document.querySelector("nav").classList.add('HeaderNav--fixed');
     // document.querySelector("nav div").classList.add('visible-logo');
-    this.document.querySelector(".HeaderNav-Placeholder").classList.add("displayPlaceholder");
+    document.querySelector(".HeaderNav-Placeholder").classList.add("displayPlaceholder");
   }
   else {
     document.querySelector("nav").classList.remove('HeaderNav--fixed');
     // document.querySelector("nav div").classList.remove('visible-logo');
-    this.document.querySelector(".HeaderNav-Placeholder").classList.remove("displayPlaceholder");
+    document.querySelector(".HeaderNav-Placeholder").classList.remove("displayPlaceholder");
 
   }
 });
