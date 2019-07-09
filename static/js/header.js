7c6fb4a2ae6e0f42808a3808c1e65ee64a7f105f
ernie-hao <pdphiro@gmail.com>
2019-07-09 11:01:16 +0100

    footer and cool svgs

--- static/js/header.js
+++ static/js/header.js
@@ -3,6 +3,8 @@ window.addEventListener('scroll', function (e) {
     document.querySelector("nav").classList.add('HeaderNav--fixed');
     // document.querySelector("nav div").classList.add('visible-logo');
     document.querySelector(".HeaderNav-Placeholder").classList.add("displayPlaceholder");
+    document.querySelector(".HeaderNav-Logo").classList.add("random-test");
+    // document.querySelector(".HeaderNav-Logo svg").style.transform = "scale(0.3)"
   }
   else {
     document.querySelector("nav").classList.remove('HeaderNav--fixed');
